50cca43a6a8c535d1995163c12c18f9a20d10601
Vladislav DUMONT <vladislav.dumont@etu.univ-amu.fr>
2026-04-27 13:24:46 +0000

    Exercice 4 complet

--- src/main/java/fr/univ_amu/iut/exercice3/PremiereScene.java
+++ src/main/java/fr/univ_amu/iut/exercice3/PremiereScene.java
@@ -19,16 +19,6 @@ public class PremiereScene extends Application {
 
   @Override
   public void start(Stage primaryStage) {
-    // TODO exercice 3 : afficher un Label au centre d'un BorderPane.
-    //
-    // 1. Créer un BorderPane (le "squelette" de la scène).
-    // 2. Créer un Label avec le texte "Bonjour, JavaFX !".
-    // 3. Placer le label au CENTRE du BorderPane (méthode setCenter).
-    // 4. Construire une Scene à partir du BorderPane.
-    // 5. Attacher la Scene au Stage.
-    // 6. Afficher le Stage.
-    //
-    // Les imports nécessaires sont déjà dans la Javadoc ci-dessus.
     BorderPane borderPane = new BorderPane();
     Label label = new Label("Bonjour, JavaFX !");
     borderPane.setCenter(label);
--- src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
+++ src/main/java/fr/univ_amu/iut/exercice4/MiseEnPage.java
@@ -1,6 +1,16 @@
 package fr.univ_amu.iut.exercice4;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 /**
@@ -34,16 +44,37 @@ public class MiseEnPage extends Application {
     // Stratégie conseillée :
     //
     // 1. Un BorderPane comme racine (setTop, setCenter, setBottom).
-    // 2. Top    : un MenuBar avec deux Menu "Fichier" et "Aide".
+    // 2. Top : un MenuBar avec deux Menu "Fichier" et "Aide".
     // 3. Center : un GridPane avec 2 lignes / 2 colonnes :
-    //                (0,0) Label "Nom :"     | (1,0) TextField
-    //                (0,1) Label "Email :"   | (1,1) TextField
+    // (0,0) Label "Nom :" | (1,0) TextField
+    // (0,1) Label "Email :" | (1,1) TextField
     // 4. Bottom : un HBox contenant deux boutons "Valider" et "Annuler",
-    //             avec un peu d'espacement et un padding.
+    // avec un peu d'espacement et un padding.
     //
     // Donne un id CSS utile sur les composants si tu veux les retrouver
     // facilement (les tests utilisent lookup sur les classes ".text-field"
     // et ".button", pas besoin d'id particulier).
+    BorderPane borderPane = new BorderPane();
+    final Menu menuFichier = new Menu("Fichier");
+    final Menu menuAide = new Menu("Aide");
+    MenuBar menuBar = new MenuBar();
+    menuBar.getMenus().addAll(menuFichier, menuAide);
+    borderPane.setTop(menuBar);
+    GridPane gridPane = new GridPane();
+    Label labelNom = new Label("Nom :");
+    Label labelEmail = new Label("Email :");
+    gridPane.add(labelNom, 0, 0);
+    gridPane.add(labelEmail, 0, 1);
+    gridPane.add(new TextField(), 1, 0);
+    gridPane.add(new TextField(), 1, 1);
+    borderPane.setCenter(gridPane);
+    HBox hBox = new HBox(20);
+    hBox.setPadding(new Insets(10));
+    hBox.getChildren().addAll(new Button("Valider"), new Button("Annuler"));
+    borderPane.setBottom(hBox);
+    Scene scene = new Scene(borderPane);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
 
   public static void main(String[] args) {
